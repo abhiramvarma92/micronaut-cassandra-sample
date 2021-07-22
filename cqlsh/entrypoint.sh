f7149414ba37e5161257b265b18d8e1bd7d7b7c4
abhivarma <abhiramvarma92@gmail.com>
2021-07-23 02:15:48 +0530

    changes for cqlsh scrips

--- cqlsh/entrypoint.sh
+++ cqlsh/entrypoint.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+#!/usr/bin/env bash
 export CQLVERSION=${CQLVERSION:-"3.4.4"}
 export CQLSH_HOST=${CQLSH_HOST:-"cassandra"}
 export CQLSH_PORT=${CQLSH_PORT:-"9042"}
 
-
+cqlsh=( cqlsh --cqlversion ${CQLVERSION} )
 
 # test connection to cassandra
 echo "Checking connection to cassandra..."
-for i in 10 20
+for i in {1..10};
 do
-  if cqlsh --cqlversion CQLSH_HOST="cassandra" ${CQLVERSION} -e "show host;" 2> /dev/null ;
+  if "${cqlsh[@]}" -e "show host;" 2> /dev/null;
   then
     break
   fi
@@ -16,11 +18,15 @@ do
   sleep $i
 done
 
+if [ "$i" = 5 ]; then
+  echo >&2 "Failed to connect to cassandra at ${CQLSH_HOST}:${CQLSH_PORT}"
+fi
+
 # iterate over the cql files in /scripts folder and execute each one
 for file in /scripts/*.cql; do
   [ -e "$file" ] || continue
   echo "Executing $file..."
-  cqlsh --cqlversion ${CQLVERSION} -f "$file"
+  "${cqlsh[@]}" -f "$file"
 done
 
 echo "Done."
